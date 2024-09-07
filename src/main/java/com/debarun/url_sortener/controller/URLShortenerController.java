9ef91cf3184a536496d02df5b489957f2ee0dcf6
Debarun Roy <debarun.dr7.roy@gmail.com>
2024-09-07 18:29:24 +0530

    added the Controller class for Shorten the URL and redirecting to Original URL and Domain Metrics for top 3 domains used to shorten

--- src/main/java/com/debarun/url_sortener/controller/URLShortenerController.java
+++ src/main/java/com/debarun/url_sortener/controller/URLShortenerController.java
@@ -1,5 +1,47 @@
 package com.debarun.url_sortener.controller;
 
-public class URLShortenerController {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.debarun.url_shortener.service.URLShortenerService;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api")
+public class URLShortenerController {
+	@Autowired
+	private URLShortenerService urlShortenerService;
+	
+	@PostMapping("/shorten")
+	public ResponseEntity<String> shortenUrl(@RequestBody String originalUrl) {
+        String shortUrl = urlShortenerService.shortenURL(originalUrl);
+        return ResponseEntity.ok(shortUrl);
+    }
+	
+	@GetMapping("/{shortUrl}")
+	public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortUrl, HttpServletResponse response) throws IOException{
+		String originalUrl = urlShortenerService.getOriginalUrl(shortUrl);
+        if (originalUrl == null) {
+            return ResponseEntity.notFound().build();
+        }
+        response.sendRedirect(originalUrl);
+        return ResponseEntity.status(HttpStatus.FOUND).build();
+	}
+	
+	@GetMapping("/metrics")
+    public ResponseEntity<List<Map.Entry<String, Integer>>> getMetrics() {
+        return ResponseEntity.ok(urlShortenerService.getTopDomains());
+    }
 }
